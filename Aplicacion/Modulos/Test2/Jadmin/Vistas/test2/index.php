7155a5754297da46eb42c07aff18e57c29374160
ever23 <enyerverfranco@gmail.com>
2019-03-07 13:41:45 -0430

    commit

--- Aplicacion/Modulos/Test2/Jadmin/Vistas/test2/index.php
+++
@@ -1,5 +0,0 @@
-<!-- Creado por Jida Framework 2019-03-07 16:58:47 -->
-<div class = "jumbotron">
-    <h2><?= $this->mensaje ?></h2>
-    <p>Use esta plantilla para iniciar de forma rápida el desarrollo de un sitio web.</p>
-</div >
