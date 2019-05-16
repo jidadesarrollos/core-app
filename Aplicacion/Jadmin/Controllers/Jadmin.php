0cce97dc2dc61474168a991e67f076ee723579ca
Alejandro Gomez <agomez@jidadesarrollos.com>
2019-05-16 11:13:53 -0400

    code

---
+++ Aplicacion/Jadmin/Controllers/Jadmin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Jadmin\Controllers;
+
+use Jida\Jadmin\Controllers\JControl;
+use Jida\Medios\Debug;
+use Jida\Medios\Sesion;
+use Jida\Render\Menu;
+
+class Jadmin extends JControl {
+
+
+
+}
--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
 
-define('DIR_JF', __DIR__);
-
 include_once 'vendor/autoload.php';
-
-$jida = new Jida\Manager(__DIR__);
-$jida->inicio();
+Jida\Manager::inicio(__DIR__);
